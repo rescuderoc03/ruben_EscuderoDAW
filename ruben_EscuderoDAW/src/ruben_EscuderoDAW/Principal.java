57aa41df7dc9d34d073e6a9e80aa0568a52d44bc
rubenescudero01 <rubenescudero01@gmail.com>
2021-03-12 16:43:46 +0100

    Líneas en el método main.

---
+++ ruben_EscuderoDAW/src/ruben_EscuderoDAW/Principal.java
@@ -0,0 +1,11 @@
+package ruben_EscuderoDAW;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Línea de visualización 1");
+		System.out.println("Línea de visualización 2");
+	}
+
+}
