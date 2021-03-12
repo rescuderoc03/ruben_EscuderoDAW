96eadcfd9df503174021960d500ae7f60daf0ffd
rubenescudero01 <rubenescudero01@gmail.com>
2021-03-12 16:49:45 +0100

    Se ha creado la rama clasenueva.

---
+++ ruben_EscuderoDAW/src/ruben_EscuderoDAW/NuevaClase.java
@@ -0,0 +1,10 @@
+package ruben_EscuderoDAW;
+
+public class NuevaClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("En la rama. Se crea la clase nueva");
+	}
+
+}
